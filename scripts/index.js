55a17860dc4849aac98ec657d0dd9f5413c69e5a
CibNumeritos <85264290+CibNumeritos@users.noreply.github.com>
2022-12-22 18:03:49 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,3 @@
-import { world, Player } from '@minecraft/server';
-
-world.events.beforeItemUse.subscribe(async (arg) => {
-    if (!arg.source instanceof Player || arg.item.typeId != 'minecraft:totem_of_undying') { return; };
-    let player = arg.source;
-    player.runCommandAsync(`testfor @s[hasitem=[{location=slot.weapon.mainhand,item=totem_of_undying,quantity=1},{location=slot.weapon.offhand,item=totem_of_undying,quantity=1}]]`).then(() => {
-      //console.warn('Cant swap')
-    }).catch(() => {
-      swap(player);
-    });
-});
-function swap(player) {
-    player.runCommandAsync(`replaceitem entity @s[hasitem=[{location=slot.weapon.mainhand,item=totem_of_undying,quantity=1},{location=slot.weapon.offhand,item=totem_of_undying,quantity=0}]] slot.weapon.offhand 0 keep totem_of_undying`);
-    player.runCommandAsync(`execute as @s[hasitem=[{location=slot.weapon.mainhand,item=totem_of_undying,quantity=1},{location=slot.weapon.offhand,item=totem_of_undying,quantity=1}]] run replaceitem entity @s[hasitem=[{location=slot.weapon.mainhand,item=totem_of_undying,quantity=1},{location=slot.weapon.offhand,item=totem_of_undying,quantity=1}]] slot.weapon.mainhand 0 air`);
-    player.runCommandAsync(`playsound armor.equip_generic @s[hasitem=[{location=slot.weapon.offhand,item=totem_of_undying,quantity=1},{location=slot.weapon.mainhand,item=totem_of_undying,quantity=0}]]`);
-};
+// Este archivo fue escrito por CibNumeritos#1094, cualquier uso, modificación o intento de lucro sin autorización del mismo sera penalizado. 
+// This file was written by CibNumeritos#1094, any use, modification or profit intent whitout their autorization will be penalized. 
+import{world,Player}from'@minecraft/server';world.events.beforeItemUse.subscribe((arg)=>{if(!arg.source instanceof Player||arg.item.typeId!='minecraft:totem_of_undying'){return;};arg.source.runCommandAsync(`execute as @s unless entity @s[hasitem=[{location=slot.weapon.mainhand,item=totem_of_undying,quantity=1},{location=slot.weapon.offhand,item=totem_of_undying,quantity=1}]] run function swap_totem`);});
