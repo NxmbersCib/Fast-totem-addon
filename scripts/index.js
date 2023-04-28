037bcde27e1ebf21eb1b161444c1ff3d003dc7ef
CibNumeritos <imcib2498.212244.2808@gmail.com>
2023-04-28 18:38:34 -0500

    v2.0.0 version: - The totem now will swap even if the offhand is already in use - Now you can swap your shield between hands - Swapped items now will preserve item data like id, name and lore - Fixed the missing hurt animation when instantly reciving damage after using a totem - Added a first "Postmortal" goal reached message! - Added a new alert that will fire in the chat when a player consumes a totem - Made the totem sound play for all the players in the world. even if they are far or in other dimension.

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,40 @@
 // Este archivo fue escrito por CibNumeritos#1094, cualquier uso, modificación o intento de lucro sin autorización del mismo sera penalizado. 
 // This file was written by CibNumeritos#1094, any use, modification or profit intent whitout their autorization will be penalized. 
-import{world,Player, EntityEquipmentInventoryComponent, EquipmentSlot, ItemStack, Container}from'@minecraft/server';
-world.events.beforeItemUse.subscribe((arg)=>{
-    if (!arg.source instanceof Player || (arg.item.typeId != 'minecraft:totem_of_undying' || arg.item.typeId != 'minecraft:shield')){
+import { world, EquipmentSlot, system } from '@minecraft/server';
+const alertColor = '§6'; // Change this to the color you want for the totem alert message
+world.events.beforeItemUse.subscribe((arg) => {
+    const { item, source } = arg;
+    if ((item.typeId != 'minecraft:totem_of_undying' && item.typeId != 'minecraft:shield')) {
         return;
     };
-    /**
-     * @type {EntityEquipmentInventoryComponent}
-     */
-    const offhand = arg.player.getComponent('minecraft:equipment_inventory');
-    /**
-     * @type {Container}
-     */
-    const container = arg.player.getComponent('minecraft:inventory').container;
-    const mainhand = container.getSlot(arg.player.selectedSlot);
-    let offhandId = offhand?.getEquipmentSlot(EquipmentSlot.offhand)?.typeId;
-    offhand.setEquipment(EquipmentSlot.offhand, mainhand.getItem());
-    mainhand.setItem(offhand.getEquipment());
-    // arg.source.runCommandAsync(`execute as @s unless entity @s[hasitem=[{location=slot.weapon.mainhand,item=totem_of_undying,quantity=1},{location=slot.weapon.offhand,item=totem_of_undying,quantity=1}]] run function swap_totem`);
+    const offhand = source.getComponent('minecraft:equipment_inventory');
+    const offhandItem = offhand.getEquipment(EquipmentSlot.offhand);
+    const container = source.getComponent('minecraft:inventory').container;
+    const mainhand = container.getSlot(source.selectedSlot);
+    const mainhandItem = container.getItem(source.selectedSlot);
+    offhand.setEquipment(EquipmentSlot.offhand, mainhandItem);
+    source.playSound('armor.equip_generic');
+    mainhand.setItem(offhandItem);
 });
+world.events.entityHurt.subscribe((arg) => {
+    const { hurtEntity, damage } = arg;
+    const health = hurtEntity.getComponent('minecraft:health')
+    if (health.current <= 0) {
+        system.runTimeout(() => {
+            if (health.current > 0) {
+                hurtEntity.runCommandAsync(`damage @s 0 none`);
+            };
+        }, 1);
+    };
+    if (damage > 0) {
+        return;
+    };
+    if (!hurtEntity.hasTag('ReachedPostmortal')) {
+        world.sendMessage({ "rawtext": [{ "text": `${hurtEntity.name} reached the goal §a[Postmortal]` }] });
+        hurtEntity.addTag('ReachedPostmortal');
+    };
+    world.sendMessage({ "rawtext": [{ "text": `${alertColor}§l${hurtEntity.name}§r${alertColor} has used a totem.` }] });
+    for (const player of world.getPlayers({ "excludeNames": [hurtEntity.name] })) {
+        player.playSound('random.totem');
+    };
+}, { "entityTypes": ["minecraft:player"] });
