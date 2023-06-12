d8b5efc359c3e06e807cf1eba1ea6fd4452fd7fa
CibNumeritos <imcib2498.212244.2808@gmail.com>
2023-06-11 21:48:03 -0500

    v2.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,33 @@
 // Este archivo fue escrito por CibNumeritos#1094, cualquier uso, modificación o intento de lucro sin autorización del mismo sera penalizado. 
 // This file was written by CibNumeritos#1094, any use, modification or profit intent whitout their autorization will be penalized. 
-import { world, EquipmentSlot, system } from '@minecraft/server';
+import { world, EquipmentSlot, system, DynamicPropertiesDefinition } from '@minecraft/server';
 const alertColor = '§6'; // Change this to the color you want for the totem alert message.
-world.events.beforeItemUse.subscribe((arg) => {
-    const { item, source } = arg;
-    if ((item.typeId != 'minecraft:totem_of_undying' && item.typeId != 'minecraft:shield')) {
+// world.afterEvents.worldInitialize.subscribe((arg) => {
+//     const definition = new DynamicPropertiesDefinition();
+//     definition.defineBoolean("fasttotem:postmortalgoal");
+//     definition.defineBoolean("fasttotem:showpostmortal");
+//     definition.defineString("fasttotem:postmortalmessage");
+//     arg.propertyRegistry.registerWorldDynamicProperties(definition); 
+// });
+world.beforeEvents.itemUse.subscribe((arg) => {
+    const { itemStack, source } = arg;
+    if ((itemStack.typeId != 'minecraft:totem_of_undying' && itemStack.typeId != 'minecraft:shield')) {
         return;
     };
-    const offhand = source.getComponent('minecraft:equipment_inventory');
-    const offhandItem = offhand.getEquipment(EquipmentSlot.offhand);
-    const container = source.getComponent('minecraft:inventory').container;
-    const mainhand = container.getSlot(source.selectedSlot);
-    const mainhandItem = container.getItem(source.selectedSlot);
-    offhand.setEquipment(EquipmentSlot.offhand, mainhandItem);
-    source.playSound('armor.equip_generic');
-    mainhand.setItem(offhandItem);
+    system.run(() => {
+        const offhand = source.getComponent('minecraft:equipment_inventory');
+        const offhandItem = offhand.getEquipment(EquipmentSlot.offhand);
+        const container = source.getComponent('minecraft:inventory').container;
+        const mainhand = container.getSlot(source.selectedSlot);
+        const mainhandItem = container.getItem(source.selectedSlot);
+        offhand.setEquipment(EquipmentSlot.offhand, mainhandItem);
+        source.playSound('armor.equip_generic');
+        mainhand.setItem(offhandItem);
+    })
 });
-world.events.entityHurt.subscribe((arg) => {
+world.afterEvents.entityHurt.subscribe((arg) => {
     const { hurtEntity, damage, damageSource } = arg;
     const health = hurtEntity.getComponent('minecraft:health');
-    if (health.current <= 0) {
-        system.runTimeout(() => {
-            if (health.current > 0) {
-                hurtEntity.runCommandAsync(`damage @s 0 override`);
-            };
-        }, 1);
-    };
     if (damage > 0 || damageSource.cause != 'none') {
         return;
     };
