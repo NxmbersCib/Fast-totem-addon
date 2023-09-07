272f0b2b4dc4096ce52e11c0d8edc51fd55ad9f1
CibNumeritos <imcib2498.212244.2808@gmail.com>
2023-09-07 16:29:53 -0500

    Primeras características (no finalizadas, no testeadas) de fast totem v3

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,20 @@ import {
   world,
   EquipmentSlot,
   system,
+  DynamicPropertiesDefinition,
 } from "@minecraft/server";
-const alertColor = "§6"; // Change this to the color you want for the totem alert message.
+const defaultValues = {
+  alert: "§6§l{name}§r§6 has used a totem.",
+  achievementMsg: "{name} reached the goal §a[Postmortal]",
+  achievement: true
+}
+world.afterEvents.worldInitialize.subscribe((arg) => {
+  const optionsDef = new DynamicPropertiesDefinition();
+  optionsDef.defineString("alert", 256, "§6§l{name}§r§6 has used a totem.");
+  optionsDef.defineBoolean("alertShow", true);
+  optionsDef.defineString("achievementMsg", 256, "{name} reached the goal §a[Postmortal]");
+  optionsDef.defineBoolean("achievementShow", true);
+})
 world.beforeEvents.itemUse.subscribe((arg) => {
   const { itemStack, source } = arg;
   if (
@@ -24,6 +36,64 @@ world.beforeEvents.itemUse.subscribe((arg) => {
     source.playSound("armor.equip_generic");
     mainhand.setItem(offhandItem);
   });
+});
+world.beforeEvents.chatSend.subscribe(async (argument) => {
+  const arg = argument;
+  await null;
+  const command = arg.message.match(/(!).*?(?=\s)/)?.[0];
+  if (!command) {
+    return;
+  }
+  const parameters = arg.message
+    .replace(command, "")
+    .match(/@"((?:[^"\\]|\\.)+)"|((?:@[^\s]+)|(?:[^"\s]+))|"([^"]+)"|{(?:[^"\s]+)/g);
+  switch (command.substring(1)) {
+    case "alert": {
+      switch (parameters[0]) {
+        case "set": {
+          world.setDynamicProperty("alert", parameters?.[1]);
+          arg.sender.sendMessage(`Totem alert message is now: ${parameters?.[1]}.`);
+        } break;
+        case "reset": {
+          world.setDynamicProperty("alert", defaultValues.alert);
+          arg.sender.sendMessage("Totem alert message was reset to its default value.");
+        } break;
+        case "show": {
+          const newValue = Boolean(parameters?.[1]);
+          if (!newValue) {
+            arg.sender.sendMessage("You must specify a new boolean (true/false) value");
+          };
+          world.setDynamicProperty("alertShow", newValue);
+        } break;
+        default: { }
+      }
+    } break;
+    case "achievement": {
+      switch (parameters[0]) {
+        case "set": {
+          world.setDynamicProperty("achievement", parameters?.[1]);
+          arg.sender.sendMessage(`Totem postmortal achievement message is now: ${parameters?.[1]}.`);
+        } break;
+        case "reset": {
+          world.setDynamicProperty("achievement", defaultValues.alert);
+          arg.sender.sendMessage("Totem postmortal achievement  message was reset to its default value.");
+        } break;
+        case "show": {
+          const newValue = Boolean(parameters?.[1]);
+          if (!newValue) {
+            arg.sender.sendMessage("You must specify a new boolean (true/false) value");
+          };
+          world.setDynamicProperty("achievementShow", newValue);
+        } break;
+        default: { }
+      }
+    } break;
+    default: {
+      player.sendMessage({
+        rawtext: [{ text: "§c" }, { translate: "commands.generic.unknown", with: [command.substring(1)] }],
+      });
+    };
+  }
 });
 world.afterEvents.entityHurt.subscribe(
   (arg) => {
